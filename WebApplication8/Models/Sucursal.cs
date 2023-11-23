cef84b5e70eb6fbeac193f624aa69da0d983a76e
Jhoan Baez <jbaez@sns.gob.do>
2023-11-23 18:38:02 -0400

    Add project files.

---
+++ WebApplication8/Models/Sucursal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    public class Sucursal
+    {
+        public int id { get; set; }
+        public String nombre { get; set; }
+        public string estado { get; set; }
+    }
+}
